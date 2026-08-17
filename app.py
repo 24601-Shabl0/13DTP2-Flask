8d9075dd2666a0608b041b3066c20511de04618a
24601-Shabl0 <24601-Shabl0>
2026-08-17 14:35:24 +1200

    Added Code

--- app.py
+++ app.py
@@ -1,14 +1,38 @@
 from flask import Flask, g
+import sqlite3 
 
 DATABASE = 'database.db'
 
-
+#initialise app
 app = Flask(__name__)
+
+def get_db():
+    db = getattr(g, '_database', None)
+    if db is None:
+        db = g._database = sqlite3.connect(DATABASE)
+    return db
+
+@app.teardown_appcontext
+def close_connection(exception):
+    db = getattr(g, '_database', None)
+    if db is not None:
+        db.close()
+
+def query_db(query, args=(), one=False):
+    cur = get_db().execute(query, args)
+    rv = cur.fetchall()
+    cur.close()
+    return (rv[0] if rv else None) if one else rv
+
 
 @app.route('/')
 def home():
-    #home page 
-    return "Chur g!"
+    #home page- just the ID, Maker, Model, and Image URL
+    sql = "SELECT * FROM cars;"
+    results = query_db(sql)
+    return str(results)
+
+
 
 if __name__ == "__main__":
     app.run(debug=True)
